48e2c17d714b53f75175a768e18ff3fb16df5e5b
Ritick Madaan <ritickmadaan57@gmail.com>
2023-05-30 10:30:28 +0530

    fix: added exhaustive types for postman collection

--- src/types/postman.rs
+++ src/types/postman.rs
@@ -1,12 +1,13 @@
+#![allow(dead_code)]
 use serde_json::Value;
 use serde::Deserialize;
 
 //NOTE the Collection type is generated in reference to
 //https://schema.postman.com/collection/json/v2.1.0/draft-07/docs/index.html
 
-#[allow(non_snake_case, dead_code)]
+#[allow(non_snake_case)]
 #[derive(Deserialize)]
-pub struct Collection {
+struct Collection {
     info: Information,
     item: Items,
     event: Option<Value>,
@@ -14,11 +15,10 @@ pub struct Collection {
     protocolProfileBehavior: Option<Value>,
 }
 
-#[allow(dead_code)]
 #[derive(Deserialize)]
-pub struct Information {
+struct Information {
     name: String,
-    _postman_id: Option<String>,
+    r#_postman_id: Option<String>,
     description: Option<Value>,
     version: Option<Value>,
     schema: Option<url::Url> //TODOP this is a mandatory field, validate url deseralization and
@@ -26,64 +26,121 @@ pub struct Information {
 }
 
 #[derive(Deserialize)]
-pub enum Items {
+enum Items {
     Item(Item),
-    Folder
+    Folder(Folder)
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct Folder {
+    name: Option<String>,
+    description: Option<Value>,
+    variable: Option<Value>,
+    item: Vec<Items>,
+    event: Vec<Value>,
+    auth: Option<Auth>,
+    protocolProfileBehavior: Option<Value>
 }
 
 #[derive(Deserialize)]
-#[allow(dead_code, non_snake_case)]
-pub struct Item {
+struct Auth {
+    r#type: AuthType,
+    noauth: Option<Value>,
+    apikey: Option<Vec<AuthAttr>>,
+    awsv4: Option<Vec<AuthAttr>>,
+    basic: Option<Vec<AuthAttr>>,
+    bearer: Option<Vec<AuthAttr>>,
+    digest: Option<Vec<AuthAttr>>,
+    edgegrid: Option<Vec<AuthAttr>>,
+    hawk: Option<Vec<AuthAttr>>,
+    ntlm: Option<Vec<AuthAttr>>,
+    oauth1: Option<Vec<AuthAttr>>,
+    oauth2: Option<Vec<AuthAttr>>,
+}
+
+#[derive(Deserialize)]
+struct AuthAttr{
+    key: String,
+    value: Option<Value>,
+    r#type: String
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Deserialize)]
+enum AuthType {
+    String(String),
+    apikey,
+    awsv4,
+    basic,
+    bearer,
+    digest,
+    edgegrid,
+    hawk,
+    noauth,
+    oauth1,
+    oauth2,
+    ntlm
+}
+
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct Item {
     id: Option<String>,
     name: Option<String>,
     description: Option<Value>,
     variable: Option<Value>,
     event: Option<Value>,
     request: Request,
-    response: Vec<Value>,//TODOP add type for this...
+    response: Option<Vec<Value>>,
     protocolProfileBehavior: Option<Value>
 }
 
 //TODOP this might be a good candidate for custom deseralization
 //If a string, the string is assumed to be the request URL and the method is assumed to be 'GET'.
 #[derive(Deserialize)]
-pub enum Request {
+enum Request {
     String(String),
-    Value(Value)//TODOP correct this
+    RequestStruct(RequestStruct)//TODOP correct this
 }
 
-pub struct RequestStruct {
+#[derive(Deserialize)]
+struct RequestStruct {
     url: Url,
-    auth: Option<Value>,//TODOP add type for this
-    proxy: Option<Value>,//TODOP ^
-    certificate: Option<Value>,//TODOP ^
+    auth: Option<Auth>,
+    proxy: Option<Value>,
+    certificate: Option<Value>,
     method: MethodEnum,
     description: Option<Value>,
     header: Header,
+    body: Body,
 }
 
-pub enum Header {
+#[derive(Deserialize)]
+enum Header {
     String(String),
     HeaderList(Vec<HeaderStruct>)
 }
 
-pub struct HeaderStruct {
+#[derive(Deserialize)]
+struct HeaderStruct {
     key: String,
     value: String,
     disabled: Option<bool>,
     description: Option<Value>,
-    body: Option<Value>//TODOP add type for this
 }
 
 //TODOP this again is a good candidate for custom deseralization
 //If object, contains the complete broken-down URL for this request.
 //If string, contains the literal request URL.
-pub enum Url {
+#[derive(Deserialize)]
+enum Url {
    String(String),
-   Value(Value)//TODOP correct this
+   UrlStruct(UrlStruct)
 }
 
-pub struct UrlStruct {
+#[derive(Deserialize)]
+struct UrlStruct {
     //The string representation of the request URL, including the protocol, host, path, hash, query parameter(s) and path variable(s).
     raw: String,
     protocol: String,
@@ -92,28 +149,31 @@ pub struct UrlStruct {
     port: String,
     query: Vec<QueryParam>,
     hash: String,
-    variable: [Value]
+    variable: Vec<Value>
 }
 
-pub enum Host {
+#[derive(Deserialize)]
+enum Host {
     String(String),
     ArrString(Vec<String>),
 }
 
-pub enum Path {
+#[derive(Deserialize)]
+enum Path {
     String(String),
     ArrString(Vec<String>)
 }
 
-pub struct QueryParam {
+#[derive(Deserialize)]
+struct QueryParam {
     key: Option<String>,
     value: Option<String>,
     disabled: bool,
     description: Value
 }
 
-
-pub enum MethodEnum {
+#[derive(Deserialize)]
+enum MethodEnum {
     GET,
     PUT,
     POST,
@@ -130,4 +190,40 @@ pub enum MethodEnum {
     PROPFIND,
     VIEW,
     String(String)
+}
+
+#[derive(Deserialize)]
+struct Body {
+    mode: Option<BodyMode>,
+    raw: String,
+    graphql: Option<Value>,
+    urlencoded: Option<Vec<UrlEncodedParam>>,
+    formdata: Option<Value>,
+    file: Option<File>,
+    options: Option<Value>,
+    disabled: Option<bool>,
+}
+
+#[derive(Deserialize)]
+struct File {
+    src: Option<String>,
+    content: Option<String>
+}
+
+#[derive(Deserialize)]
+struct UrlEncodedParam {
+    key: String,
+    value: Option<String>,
+    disabled: Option<bool>,
+    description: Option<Value>
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Deserialize)]
+enum BodyMode {
+    raw,
+    urlencoded,
+    formdata,
+    file,
+    graphql,
 }
